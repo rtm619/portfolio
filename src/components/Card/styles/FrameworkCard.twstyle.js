9e33c8720b37d87028c8bdaf95ea0e6ca8492796
Reetam Chatterjee <rtm619@gmail.com>
2020-05-12 22:04:37 +0530

    Fixed Framework Card Button style

--- src/components/Card/styles/FrameworkCard.twstyle.js
+++ src/components/Card/styles/FrameworkCard.twstyle.js
@@ -6,5 +6,5 @@ export default {
   shortDescription: 'text-white px-10 font-SpartanRegular text-12 mdl:text-14 text-center mb-15',
   link: 'frameworkcard-button mx-auto flex mdl:w-1/3 w-1/2 relative hover:bg-orange',
   linkText: 'text-white font-SpartanMedium text-14 w-full text-center py-5',
-  linkIcon: 'text-transparent fill-current absolute right-0',
+  linkIcon: 'text-transparent w-20 h-20 fill-current absolute right-0',
 };
