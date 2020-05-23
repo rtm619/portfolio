d9be591ac0fbf03e04ce4f3138f980e7ff7c91ad
Reetam Chatterjee <rtm619@gmail.com>
2020-05-23 21:16:29 +0530

    Fixed iPhone issue for Animations

--- src/components/MotionPathSection/styles/Content.twstyle.js
+++ src/components/MotionPathSection/styles/Content.twstyle.js
@@ -1,26 +1,26 @@
 export default {
   default: {
-    wrapper: 'w-full flex',
+    wrapper: 'w-full block',
     item: '',
   },
   'Image-V1': {
-    wrapper: 'w-full flex',
+    wrapper: 'w-full block',
     item: 'mdl:w-1/3 w-full object-cover mx-auto border border-black rounded-lg h-auto',
   },
   'Cutout-V1': {
-    wrapper: 'w-full absolute top-0 left-0 right-0 flex',
+    wrapper: 'w-full absolute top-0 left-0 right-0 block',
     item: 'mdl:w-1/3 w-full mx-auto',
   },
   'Text-V1': {
-    wrapper: 'w-full flex',
+    wrapper: 'w-full block',
     item: 'mdl:w-1/3 w-full mx-auto my-60 text-48 font-SpartanBold text-center text-white',
   },
   'Image-V2': {
-    wrapper: 'w-full flex',
+    wrapper: 'w-full block',
     item: 'mdl:w-1/5 w-4/5 mx-auto object-cover border border-black rounded-lg h-auto',
   },
   'Cutout-V2': {
-    wrapper: 'w-full absolute top-0 left-0 right-0 flex',
+    wrapper: 'w-full absolute top-0 left-0 right-0 block',
     item: 'mdl:w-1/5 w-4/5 mx-auto h-auto',
   },
 };
