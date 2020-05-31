b02c6f18ec2ba696fd70c714bdca7ea17e4a1e38
Reetam Chatterjee <rtm619@gmail.com>
2020-05-31 22:17:37 +0530

    style spell error fix

--- src/components/SocialMediaSection/styles/SocialMediaSection.twstyle.js
+++ src/components/SocialMediaSection/styles/SocialMediaSection.twstyle.js
@@ -2,7 +2,7 @@ export default {
   wrapper: 'flex flex-wrap mb-60 mt-20',
   socialCardsWrapper: 'w-full flex justify-center',
   contactUsWrapper: 'w-full',
-  contactUsTitle: 'w-full text-center text-32 tfont-SpartanMedium text-white py-40',
+  contactUsTitle: 'w-full text-center text-32 font-SpartanMedium text-white py-40',
   mailButton: 'mail-btn h-60 relative block max-w-sm mx-20 text-center mdl:mx-auto overflow-hidden',
   mailButtonText: 'z-1 text-24 font-SpartanMedium w-full text-black mdl:text-white absolute top-0 bottom-0 flex justify-center items-center',
   mailButtonIcon: 'text-orange fill-current w-full h-full',
