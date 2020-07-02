ac9eb565fe434e2aca584380d78304fe12bc4ae7
Reetam Chatterjee <rtm619@gmail.com>
2020-07-02 22:32:43 +0530

    Added bg color for img tag to show white bg

--- src/components/Banner/styles/Banner.twstyle.js
+++ src/components/Banner/styles/Banner.twstyle.js
@@ -2,7 +2,7 @@ export default {
   default: {
     wrapper: 'w-full relative',
     imgWrapper: 'w-full',
-    img: 'mx-auto w-full min-h-screen object-cover',
+    img: 'mx-auto w-full min-h-screen bg-white object-cover',
     contentWrapper: 'absolute mdl:top-25% left-0 bottom-0 md:pb-50 mdl:pb-0 mdl:w-1/2 w-full mdl:pl-50 mdl:pr-0 px-30 fade-in opacity-0',
     title: 'text-18 md:text-32 mdl:text-18 text-white mdl:text-black mdl:font-SpartanMedium font-SpartanRegular mb-20 mdl:mb-30',
     shortDescription: 'text-18 md:text-40 mdl:text-22 text-white mdl:text-black mdl:font-SpartanBold font-SpartanMedium mb-30 mdl:mb-40 leading-relaxed',
